8e702a8e69a79208d8880a49a62bc9c20f4beca6
Milan Todorovic <todor-90@hotmail.com>
2018-05-17 16:58:21 +0200

    Add files via upload

---
+++ interface.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+import baza
+
+# lista polja za autore, za svaki elemnet u toj listi pokrenuti for-loop za insert authori rezultat cuvati u listi
+
+# insert_authors vraca INT!!!
+
+# check input before pressing SUBMIT
